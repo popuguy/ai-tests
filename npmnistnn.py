119cd9162002f53859099aa99b32058153a2ef76
Paul Cantrell <paulcantr@gmail.com>
2017-07-14 21:40:33 -0400

    Cleaned up

--- npmnistnn.py
+++ npmnistnn.py
@@ -7,6 +7,9 @@ import matplotlib
 
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
+np.set_printoptions(precision=3)
+np.set_printoptions(suppress=True)
+
 def display_mnist(img, label):
     '''Visually display the 28x28 unformatted array
     '''
@@ -27,6 +30,7 @@ def convert_digit_to_onehot(digit):
     return [0] * digit + [1] + [0] * (9 - digit)
 
 images = mnist.train.images
+# images = np.add(images, 0.1)
 labels = mnist.train.labels
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -38,9 +42,14 @@ syn1 = 2 * np.random.random((dimension**2, hidden_layer_1_num_nodes)) - 1
 syn2 = 2 * np.random.random((hidden_layer_1_num_nodes, hidden_layer_2_num_nodes)) - 1
 syn3 = 2 * np.random.random((hidden_layer_2_num_nodes, hidden_layer_3_num_nodes)) - 1
 syn4 = 2 * np.random.random((hidden_layer_3_num_nodes, output_layer_num_nodes)) - 1
+testing = False
+test_n = 3
 for iter in range(full_iterations):
     print('Epic epoch bro, we\'re at #' + str(iter+1))
     for section in range(0, len(images), batch_size):
+        if testing:
+            print('Syn before',syn1)
+
         training_images = images[section:section+batch_size]
         training_labels = labels[section:section+batch_size]
         l0 = training_images
@@ -56,17 +65,42 @@ for iter in range(full_iterations):
         l2_delta = l2_err * slope_from_sigmoid(l2)
         l1_err = np.dot(l2_delta, syn2.T)
         l1_delta = l1_err * slope_from_sigmoid(l1)
-        syn4 += np.dot(l3.T, l4_delta)
-        syn3 += np.dot(l2.T, l3_delta)
-        syn2 += np.dot(l1.T, l2_delta)
-        syn1 += np.dot(l0.T, l1_delta)
+        syn4_update = np.dot(l3.T, l4_delta)
+        syn4 += syn4_update
+        syn3_update = np.dot(l2.T, l3_delta)
+        syn3 += syn3_update
+        syn2_update = np.dot(l1.T, l2_delta)
+        syn2 += syn2_update
+        syn1_update = np.dot(l0.T, l1_delta)
+        syn1 += syn1_update
+        if testing:
+            print('Syn after',syn1)
+            print('Due to syn1 update', syn1_update)
+            print('Number non-zero elems', len(syn1_update.nonzero()))
+            print('Which were', syn1_update.nonzero())
+            print('From the l1_delta', l1_delta)
+            print(l0[0:test_n])
+            print("----------")
+            print(l1[0:test_n])
+            print("----------")
+            print(l2[0:test_n])
+            print("----------")
+            print(l3[0:test_n])
+            print("----------")
+            print(l4[0:test_n])
+            print("----------")
+            print(training_labels[0:test_n])
+            a=input()
+            if len(a) > 0 and a[0]=='s':
+                testing=False
 correct = 0
 total = 0
 l4list = l4.tolist()
 training_labelslist = training_labels.tolist()
 print('Num things', len(l4list))
 for i in range(len(l4list)):
-    print(l4list[i])
+    print(["{0:0.2f}".format(a) for a in l4list[i]])
+    # print(l4list[i])
     # display_mnist(l0[i], str(l4list[i].index(max(l4list[i]))))
     if l4list[i].index(max(l4list[i])) == training_labelslist[i].index(max(training_labelslist[i])):
         correct += 1
